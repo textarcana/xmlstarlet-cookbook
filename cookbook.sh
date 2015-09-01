0cf5716699ecd0ed8faaa3a543a0af4eb61af832
textarcana <noah@onemorebug.com>
2015-09-01 13:54:34 -0400

    How to dump the schema of an XML document
    -a is the secret sauce here! Without that, attributes wouldn't be printed out!

--- cookbook.sh
+++ cookbook.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+## How to dump the schema of an XML document:
+
+xmlstarlet el -a example/jenkins/build.xml | sort | uniq
+
 ## How to find and print an element whose text matches a string:
 
 xmlstarlet sel -t -m '//hudson.model.TextParameterValue[name="bar"]' \
